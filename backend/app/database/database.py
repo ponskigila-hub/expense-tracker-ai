692fb0086cd4193e0dfe6e1d3fb343bec6189e77
Ponski <ponskigila@gmail.com>
2026-07-29 21:00:21 +0700

    Configuration Management

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from app.config import settings
 
 DATABASE_URL = "sqlite:///expense_tracker.db"
 
 engine = create_engine(
-    DATABASE_URL,
+    settings.DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
