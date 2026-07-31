0e59b467765a18e18737e04a09468ddcb29e0fd6
Ponski <ponskigila@gmail.com>
2026-07-31 17:12:29 +0700

    add login and register

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
+from fastapi import Depends
+
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
 
 from app.schemas.user import (
     UserCreate,
-    UserResponse
+    UserResponse,
+    UserLogin,
+    Token
 )
 
 from app.services.user_service import UserService
-
 
 router = APIRouter(
     tags=["Authentication"]
@@ -29,4 +32,19 @@ def register(
     return UserService.register_user(
         db,
         user
+    )
+
+
+@router.post(
+    "/login",
+    response_model=Token
+)
+def login(
+    credentials: UserLogin,
+    db: Session = Depends(get_db)
+):
+
+    return UserService.login(
+        db,
+        credentials
     )
